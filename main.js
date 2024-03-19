95ab714e34c18567d8325e9394ef491d8ca45eec
PavelasB <60621475+paskaeu25@users.noreply.github.com>
2024-03-19 18:11:02 +0200

    now squares change color on hover

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
-const boxContainer = document.querySelector("#boxes");
+const createGrid = () => {
+  const boxContainer = document.querySelector('#boxes');
 
-for (let i = 1; i <= 16; i++) {
-  for (let j = 1; j <= 16; j++) {
-    const box = document.createElement("div");
-    box.classList.add("box");
-    boxContainer.appendChild(box);
+  for (let i = 1; i <= 16; i++) {
+    for (let j = 1; j <= 16; j++) {
+      const box = document.createElement('div');
+      box.classList.add('box');
+      boxContainer.appendChild(box);
+    }
   }
-}
+};
+
+createGrid();
+
+const changeColors = () => {
+  const allBoxes = document.querySelectorAll('.box');
+
+  allBoxes.forEach((box) => {
+    box.addEventListener(
+      'mouseover',
+      (e) => (e.target.style.backgroundColor = 'red')
+    );
+  });
+};
+
+changeColors();
