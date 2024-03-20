2a38cbd90c3303ae66cb39a80b292c87f91f7e76
Pavelas Bulatovas <BULA@danskebank.lt>
2024-03-20 16:54:19 +0200

    made changes so grid with any number of boxes can be created and grid stays squared

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const createGrid = (numberOfBoxes) => {
-  const boxContainer = document.querySelector('#boxes');
+  const boxContainer = document.querySelector("#boxes");
+
+  const boxSize = 800 / numberOfBoxes + "px";
 
   for (let i = 1; i <= numberOfBoxes; i++) {
     for (let j = 1; j <= numberOfBoxes; j++) {
-      const box = document.createElement('div');
-      box.classList.add('box');
+      const box = document.createElement("div");
+      box.classList.add("box");
+      box.style.width = boxSize;
+      box.style.height = boxSize;
+
       boxContainer.appendChild(box);
     }
   }
@@ -13,12 +18,12 @@ const createGrid = (numberOfBoxes) => {
 createGrid(10);
 
 const changeColors = () => {
-  const allBoxes = document.querySelectorAll('.box');
+  const allBoxes = document.querySelectorAll(".box");
 
   allBoxes.forEach((box) => {
     box.addEventListener(
-      'mouseover',
-      (e) => (e.target.style.backgroundColor = 'red')
+      "mouseover",
+      (e) => (e.target.style.backgroundColor = "red")
     );
   });
 };
